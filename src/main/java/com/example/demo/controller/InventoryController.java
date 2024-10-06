182249c5a5524213f1563ec164084207f0b3cab2
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-10-06 13:11:21 +0530

    updated add item in inventoryControloler

--- src/main/java/com/example/demo/controller/InventoryController.java
+++ src/main/java/com/example/demo/controller/InventoryController.java
@@ -1,4 +1,29 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.InventoryDTO;
+import com.example.demo.model.Inventory;
+import com.example.demo.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1")
 public class InventoryController {
+    @Autowired
+    private InventoryService inventoryService;
+
+    @GetMapping("/getitems")
+    public List<InventoryDTO> getItems() {
+        return inventoryService.getAllItems();
+    }
+
+    @PostMapping("/additem")
+    public InventoryDTO saveItem(@RequestBody InventoryDTO inventoryDTO) {
+        return inventoryService.saveItem(inventoryDTO);
+    }
+
+
 }
