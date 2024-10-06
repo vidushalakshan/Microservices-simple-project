9bc80fa4da5600040a6ee77ca9b40a092c8ad1ad
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-10-06 12:58:36 +0530

    updated save item and getAllItems in InventoryService

--- src/main/java/com/example/demo/repo/InventoryRepo.java
+++ src/main/java/com/example/demo/repo/InventoryRepo.java
@@ -1,4 +1,11 @@
 package com.example.demo.repo;
 
-public interface InventoryRepo {
+import com.example.demo.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepo extends JpaRepository<Inventory, Integer> {
+
+
 }
--- src/main/java/com/example/demo/service/InventoryService.java
+++ src/main/java/com/example/demo/service/InventoryService.java
@@ -1,4 +1,34 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.InventoryDTO;
+import com.example.demo.model.Inventory;
+import com.example.demo.repo.InventoryRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.management.modelmbean.ModelMBean;
+import java.util.List;
+
+@Service
+@Transactional
 public class InventoryService {
+    @Autowired
+    private InventoryRepo inventoryRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<InventoryDTO> getAllItems() {
+        List<Inventory> itemList = inventoryRepo.findAll();
+        return modelMapper.map(itemList, new TypeToken<List<InventoryDTO>>(){}.getType());
+    }
+
+    public InventoryDTO saveItem(InventoryDTO inventoryDTO) {
+        inventoryRepo.save(modelMapper.map(inventoryDTO, Inventory.class));
+        return inventoryDTO;
+    }
+
 }
